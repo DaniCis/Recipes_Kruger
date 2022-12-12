bccb146342b43d24cc4934c6f9a6046cf766159d
DaniCis <danicisneros2@hotmail.com>
2022-12-11 19:25:12 -0500

    changes in detail page

--- src/recipies/SearchRecipe.jsx
+++ src/recipies/SearchRecipe.jsx
@@ -67,55 +67,54 @@ export default function DetailsRecipies() {
 			{loading 
 				? <Loading />
 				: (
-					<div className="flex items-center justify-center w-full min-h-screen">
-						<div className="flex flex-col w-full max-w-4xl p-10 space-y-6 text-white shadow-lg md:flex-row md:space-x-6 md:space-y-0 rounded-xl max-sm:w-auto max-sm:my-5">
+					<div className="flex items-center justify-center my-4">
+						<div className="flex flex-col sm:w-full max-w-4xl p-10 space-y-6 shadow-lg md:flex-row md:space-x-6 md:space-y-0 rounded-xl max-sm:w-70 max-sm:my-5 bg-gray-100">
 							<div className="flex flex-col  p-8 space-y-8 text-gray-600 bg-white shadow-lg rounded-xl w-8/12">
-								<div className="my-5">
-									<h3 className="text-lg font-bold">{infos.label}</h3>
+								<div className="my-2">
+									<h3 className="text-xl font-bold text-center">{infos.label}</h3>
 								</div>
 								<div className="flex justify-center">
 									<img
 										src={infos.image}
 										alt=""
-										className="mask mask-squircle w-56"
+										className="mask mask-square rounded-xl w-48"
 									/>
 								</div>
-								<div className="py-10">
+								<div className="py-4">
 									<div className="flex flex-row gap-2">
-										<p>Porciones: </p>
-										<p>{infos.yield}</p> 
+										<p><span className="font-bold text-xl text-green-500">{infos.yield} </span> 
+										servings</p>  
 									</div>
 									<div className="flex flex-row gap-2">
-										<p >Tipo de Cocina: </p>
-										<p>{infos.cuisineType}</p>
+										<p><span className="font-bold text-xl text-red-300">{infos.calories} </span>calories</p>
 									</div>
 									<div className="flex flex-row gap-2">
-										<p>Tipo de Comida: </p>
-										<p>{infos.mealType}</p>
+										<p className="font-semibold">Cuisine Type: </p>
+										<p className="capitalize">{infos.cuisineType}</p>
 									</div>
 									<div className="flex flex-row gap-2">
-										<p>Calorias: </p>
-										<p>{infos.calories}</p>
+										<p className="font-semibold">Meal Type: </p>
+										<p className="capitalize">{infos.mealType}</p>
 									</div>
 								</div>
 							</div>
-							<div className="w-10/12 p-8 text-gray-600 bg-white shadow-lg rounded-xl max-sm:w-96">
+							<div className="w-10/12 p-8 text-gray-600 bg-white shadow-lg rounded-xl max-sm:w-80">
 								<div className="flex flex-col space-y-4">
 									<div className="flex justify-center border-b-2">
 										<h1 className="my-2 text-lg font-bold">Details</h1>
 									</div>
 									<div className="flex flex-row space-x-3">
-										<p>Author: </p>{" "}
+										<p className="font-semibold">Author: </p>{" "}
 										<p> {infos.source} </p>
 									</div>
 									<div>
 										<p>
-											Ver receta completa <a href={infos.url}>aqui</a>
+											See full preparation in <a className="link link-primary" href={infos.url}>here</a>
 										</p>
 									</div>
 									<div className="divider"></div>
 									<div>
-										<p className="mx-2 mb-2">Ingredients: </p>
+										<p className="mx-2 mb-2 font-semibold">Ingredients: </p>
 										<ul>
 											{infos.ingredients?.map((recipe, index) => (
 												<li className="list-disc list-inside" key={index}>
