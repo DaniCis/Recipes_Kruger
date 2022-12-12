758b4ee3388d23731d7827f40cc2ae4c7c1844cf
DaniCis <danicisneros2@hotmail.com>
2022-12-12 10:03:11 -0500

    final changes

--- src/recipies/components/Navbar.jsx
+++ src/recipies/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () =>{
                 </ul>
                 </div>
                 <img src={logo} className="w-12 mx-2" alt='logo'/>
-                <p className="font-medium text-xl">CookBook</p>
+                <p className="font-semibold text-xl">CookBook</p>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -50,7 +50,7 @@ const Navbar = () =>{
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="avatar placeholder">
                         <div className=" cursor-pointer bg-info text-neutral-content rounded-full w-12 ">
-                            <span className="text-xl ">{displayName.substring(0,2)}</span>
+                            <span className="font-semibold text-xl ">{displayName.substring(0,2)}</span>
                         </div>
                     </label> 
                     <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
